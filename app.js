b52772263263ff087cb4b08c0b9eddea5ff8d8e9
ror2022 <fotoappkodemia@gmail.com>
2023-04-16 18:04:46 -0600

    agregando cors

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { routes } from "./routes/routes.js";
+import cors from 'cors';
 
 export const app = express();
 
 app.use("/api/", routes);
+app.use(cors());
