849491ec973ab04c6771138499465a692ab8f85c
ror2022 <fotoappkodemia@gmail.com>
2023-04-18 20:51:04 -0600

    agregando rutas y controllers entidad albums

--- controllers/album.controller.js
+++ controllers/album.controller.js
@@ -10,3 +10,23 @@ export const getAllAlbumes = (req,res)=>{
 export const createAlbum = (req,res)=>{
     return res.status(200).json("Me crea un registro de album, y me devuelve el registro del album creado")
 }
+
+//Me devuelve la lista de albums del usuario
+export const getMyAlbums = (req,res)=>{
+    return res.status(200).json("Recupera los Albumes de un Usuario Fotografo:..")
+}
+
+//Recupera el album especificado
+export const getOneAlbum = (req,res)=>{
+    return res.status(200).json("Recupera un Album por id ..")
+}
+
+//Actualiza un album
+export const updateAlbum = (req,res)=>{
+    return res.status(200).json("Actualiza un Album por id ..")
+}
+
+//Elimina un album
+export const deleteAlbum = (req,res)=>{
+    return res.status(200).json("Elimina un Album por id ..")
+}
--- routes/album.routes.js
+++ routes/album.routes.js
@@ -1,8 +1,12 @@
 import {Router} from "express";
-import { createAlbum, getAllAlbumes } from "../controllers/album.controller.js";
+import { createAlbum, getAllAlbumes, getMyAlbums, getOneAlbum, updateAlbum, deleteAlbum } from "../controllers/album.controller.js";
 
 
 
 export const albumRoutes = Router()
                             .get('/allAlbums',getAllAlbumes)
                             .post('/',createAlbum)
+                            .get('/myAlbums/:idPhotoUser',getMyAlbums)
+                            .get('/:id',getOneAlbum)
+                            .put('/:id',updateAlbum)
+                            .delete('/:id',deleteAlbum)
