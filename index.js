69bf7a3ee6c122e888a4ac3b2c88366dfda02716
ror2022 <fotoappkodemia@gmail.com>
2023-05-03 16:56:21 -0600

    cambio back

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 import { app } from "./app.js";
 import { PORT } from "./config.js";
 import { connectDB } from "./db/db.js";
-
-
+//import { setCorsBucket } from "./lib/album.lib.js";
 
 connectDB();
 
 app.listen(PORT);
 
-console.log("Server is running on port:", PORT); 
+console.log("Server is running on port:", PORT);
 
-
-
-//await myS3PutObject();
-
-//await putCorsPolicy();
+//await setCorsBucket();
 
 //version base del backend
 //iniciamos con la rama dev
